e78f258632fee1d2632d20713c1f0d1ec9e1667e
m3rg <esenyy@gmail.com>
2020-02-27 09:09:22 +0300

    Problem 80, Remove Duplicates from Sorted Array II

---
+++ src/main/java/com/m3rg/leetcode_problems/medium/_80_RemoveDuplicatesFromSortedArrayII.java
@@ -0,0 +1,82 @@
+/**
+ * Given a sorted array nums, remove the duplicates in-place such that duplicates appeared at most twice and return the new length.
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+Example 1:
+
+Given nums = [1,1,1,2,2,3],
+
+Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3 respectively.
+
+It doesn't matter what you leave beyond the returned length.
+Example 2:
+
+Given nums = [0,0,1,1,1,1,2,3,3],
+
+Your function should return length = 7, with the first seven elements of nums being modified to 0, 0, 1, 1, 2, 3 and 3 respectively.
+
+It doesn't matter what values are set beyond the returned length.
+Clarification:
+
+Confused why the returned value is an integer but your answer is an array?
+
+Note that the input array is passed in by reference, which means modification to the input array will be known to the caller as well.
+
+Internally you can think of this:
+
+// nums is passed in by reference. (i.e., without making a copy)
+int len = removeDuplicates(nums);
+
+// any modification to nums in your function would be known by the caller.
+// using the length returned by your function, it prints the first len elements.
+for (int i = 0; i < len; i++) {
+    print(nums[i]);
+}
+
+https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
+ */
+package com.m3rg.leetcode_problems.medium;
+
+import com.m3rg.leetcode_problems.utils.ListUtils;
+
+public class _80_RemoveDuplicatesFromSortedArrayII {
+
+    public static void main(String[] args) {
+        _80_RemoveDuplicatesFromSortedArrayII obj = new _80_RemoveDuplicatesFromSortedArrayII();
+        int[] nums = new int[]{1, 1, 1, 2, 2, 3};
+        int result = obj.removeDuplicates(nums);
+        System.out.println(result);
+        ListUtils.printArray(nums);
+    }
+
+    public int removeDuplicates(int[] nums) {
+        int i = 0;
+        for (int num : nums) {
+            if (i < 2 || num > nums[i - 2]) {
+                nums[i++] = num;
+            }
+        }
+        return i;
+    }
+
+    public int solution2(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int count = 1;
+        int duplicate = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if(nums[i] == nums[i - 1]) {
+                duplicate++;
+                if(duplicate > 2) {
+                    continue;
+                }
+            } else {
+                duplicate = 1;
+            }
+            nums[count++] = nums[i];
+        }
+        return count;
+    }
+}
